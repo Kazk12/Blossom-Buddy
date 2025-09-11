efc50ffc947f8ea9e3afb56ee25d84098ae8082e
Kaz <kazuko43KO@gmail.com>
2025-09-11 14:53:20 +0200

    Ajout de migration et model pour plant Ajout de la relation dans les model de plant et user pour la relation many to many

---
+++ app/Models/Plant.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Plant extends Model
+{
+    public $fillable = [
+        'common_name',
+        'watering_general_benchmark'
+    ];
+
+    public $casts = [
+        'watering_general_benchmark' => 'array'
+    ];
+
+    public function users(){
+        return $this->belongsToMany(
+            User::class
+        );
+    }
+}
