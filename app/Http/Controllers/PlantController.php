592d8559e60ec7a809aef218774705cd8de40c9e
Kaz <kazuko43KO@gmail.com>
2025-09-12 12:48:51 +0200

    mise en place de la documentation swagger sur les controller

--- app/Http/Controllers/PlantController.php
+++ app/Http/Controllers/PlantController.php
@@ -5,11 +5,23 @@ namespace App\Http\Controllers;
 use App\Models\Plant;
 use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
+use OpenApi\Annotations as OA;
+
 
 
 class PlantController extends Controller
 {
     use HttpResponses;
+
+    /**
+ * @OA\Get(
+ *     path="/plant",
+ *     summary="Get a list of plants",
+ *     tags={"Plants"},
+ *     @OA\Response(response=200, description="Successful operation"),
+ *     @OA\Response(response=400, description="Invalid request")
+ * )
+ */
 
     public function index(Request $request){
 
@@ -17,6 +29,24 @@ class PlantController extends Controller
         return $this->success($plants);
 
     }
+
+    /**
+ * @OA\Post(
+ *     path="/plant",
+ *     summary="Create a new plant",
+ *     @OA\RequestBody(
+ *         required=true,
+ *         @OA\JsonContent(
+ *             required={"common_name","watering_general_benchmark"},
+ *             @OA\Property(property="common_name", type="string"),
+ *             @OA\Property(property="watering_general_benchmark", type="array", @OA\Items(type="string"))
+ *         )
+ *     ),
+ *     tags={"Plants"},
+ *     @OA\Response(response=200, description="Successful operation"),
+ *     @OA\Response(response=400, description="Invalid request")
+ * )
+ */
 
     public function store(Request $request){
 
@@ -32,6 +62,19 @@ class PlantController extends Controller
 
     }
 
+       /**
+ * @OA\Get(
+ *     path="/plant/{name}",
+ *     summary="Get a specific plant by name",
+ *     parameters={
+ *         @OA\Parameter(name="name", in="path", required=true, @OA\Schema(type="string")),
+ *     },
+ *     tags={"Plants"},
+ *     @OA\Response(response=200, description="Successful operation"),
+ *     @OA\Response(response=404, description="Plant not found")
+ * )
+ */
+
     public function show($name){
 
         $plant = Plant::where('common_name', 'LIKE', "%{$name}%")->first();
@@ -41,6 +84,19 @@ class PlantController extends Controller
         return $this->success($plant);
 
     }
+
+         /**
+ * @OA\Delete(
+ *     path="/plant/{id}",
+ *     summary="Delete a specific plant",
+ *     parameters={
+ *         @OA\Parameter(name="id", in="path", required=true, @OA\Schema(type="integer")),
+ *     },
+ *     tags={"Plants"},
+ *     @OA\Response(response=200, description="Successful operation"),
+ *     @OA\Response(response=404, description="Plant not found")
+ * )
+ */
 
     public function destroy($id){
 
--- app/Http/Controllers/UserPlantController.php
+++ app/Http/Controllers/UserPlantController.php
@@ -5,11 +5,24 @@ namespace App\Http\Controllers;
 use App\Models\Plant;
 use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
+use OpenApi\Annotations as OA;
+
 
 
 class UserPlantController extends Controller
 {
     use HttpResponses;
+
+        /**
+     * @OA\GET(
+     *     path="/user/plants",
+     *     summary="Get a list of plants for the authenticated user",
+     *     parameters={},
+     *     tags={"User Plants"},
+     *     @OA\Response(response=200, description="Successful operation"),
+     *     @OA\Response(response=401, description="Unauthorized")
+     * )
+     */
 
     public function index(Request $request){
 
@@ -19,6 +32,20 @@ class UserPlantController extends Controller
         return $this->success($plants);
 
     }
+
+    /**
+     * @OA\POST(
+     *     path="/user/plant",
+     *     summary="Create a new plant for the authenticated user",
+     *    parameters={
+     *         @OA\Parameter(name="common_name", in="query", required=true, @OA\Schema(type="string")),
+     *         @OA\Parameter(name="watering_general_benchmark", in="query", required=true, @OA\Schema(type="array", @OA\Items(type="string"))),
+     *     },
+     *     tags={"User Plants"},
+     *     @OA\Response(response=201, description="Plant created successfully"),
+     *     @OA\Response(response=401, description="Unauthorized")
+     * )
+     */
 
        public function store(Request $request){
 
@@ -39,6 +66,19 @@ class UserPlantController extends Controller
 
         return $this->success($plant, "Plant succesfully created by user " . $user->name, 201);
     }
+
+    /**
+     * @OA\DELETE(
+     *     path="/user/plant/{id}",
+     *     summary="Delete a plant for the authenticated user",
+     *    parameters={
+     *         @OA\Parameter(name="id", in="path", required=true, @OA\Schema(type="integer")),
+     *     },
+     *     tags={"User Plants"},
+     *     @OA\Response(response=200, description="Plant deleted successfully"),
+     *     @OA\Response(response=401, description="Unauthorized")
+     * )
+     */
 
     public function destroy($id, Request $request){
 
